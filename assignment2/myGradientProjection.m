5d6c95841c7dd24fc0bc77b4b81bbf64f9e971c8
Fin <Fin.Yang@monash.edu>
2019-10-19 19:14:15 +1100

    finished assignment 2 Q3; finished assignment 2

--- assignment2/myGradientProjection.m
+++ assignment2/myGradientProjection.m
@@ -35,4 +35,19 @@ function [x,step]=myGradientProjection(f,gf,projfun,x,beta,sigma,epstol,maxstep)
 % WRITE YOUR CODE BELOW THIS LINE!
 %--------------------------------------------------------------------------
 
+k = 0;
+xk = @(t) projfun(x-t*gf(x));
+while(norm(xk(1)-x) > epstol && k< maxstep)
+    k = k+1;
+    l = 0;
+    while(f(xk(beta^l))> f(x)-sigma*gf(x)' * (x-xk(beta^l)))
+       l = l+1; 
+    end
+    t = beta^l;
+    
+    x = xk(t);
+    
+end
+step=k;
+end
         
